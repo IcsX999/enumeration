5dbb5d7e38e11a06e1c640fdaec7e41f1a9e384c
root <root@localhost.localdomain>
2019-10-27 14:31:02 +0000

    remove gobuster file

--- gobuster_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "[*] Host: " host
-read -p "[*] Seclist path: " seclist_path
-read -p "[*] Extentions:" ext
-/usr/bin/gobuster dir -u "$host" -w "$seclist_path/Discovery/Web-Content/common.txt" -t 25 -o common-1 -x "$ext"
-/usr/bin/gobuster dir -u "$host" -w "$seclist_path/Discovery/Web-Content/big.txt" -t 25 -o big-1 -x "$ext"
-/usr/bin/gobuster dir -u "$host" -w "$seclist_path/Discovery/Web-Content/directory-list-2.3-medium.txt" -t 25 -o dirmed-1 -x "$ext"
