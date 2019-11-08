665c2dae4af7574f58bd0902be981bdb6e4a6d7d
IcsX999 <IcsX999>
2019-11-08 11:37:43 +0100

    fix color host_scan

--- host_scan.sh
+++ host_scan.sh
@@ -5,26 +5,9 @@ usage="$(basename $0) -a <all|normal> -h <host>"
 # Reset                         
 Color_Off='\e[0m'       # Text Reset
                                
-# Regular Colors               
-Black='\e[0;30m'        # Nero 
-Red='\e[0;31m'          # Rosso 
-Green='\e[0;32m'        # Verde 
-Yellow='\e[0;33m'       # Giallo
-Blue='\e[0;34m'         # Blu   
-Purple='\e[0;35m'       # Viola 
-Cyan='\e[0;36m'         # Ciano     
-White='\e[0;37m'        # Bianco                    
-                         
 # Bold                        
-BBlack='\e[1;30m'       # Nero 
 BRed='\e[1;31m'         # Rosso
 BGreen='\e[1;32m'       # Verde 
-BYellow='\e[1;33m'      # Giallo
-BBlue='\e[1;34m'        # Blu        
-BPurple='\e[1;35m'      # Viola
-BCyan='\e[1;36m'        # Ciano 
-BWhite='\e[1;37m'       # Bianco
-
 
 while getopts a:h: option
 do
@@ -41,9 +24,9 @@ scan () {
 	ports=$(echo "$out" |grep open |awk -F/ '{print $1}' |tr '\n' ',' |sed s'/.$//')
 	if [ -z $ports ]
 	then
-		echo "${BRed}[!] no TCP ports open${Color_Off}"
-		echo "${BRed}[-] try with UDP${Color_Off}"
-		/usr/bin/nmap -T5 -sU $host
+		echo -e "${BRed}[!] no TCP ports open${Color_Off}"
+		echo -e "${BRed}[-] try with UDP${Color_Off}"
+		/usr/bin/sudo usr/bin/nmap -T5 -sU $host
 	else
 		echo -e "${BGreen}[*] Starting NMAP Service Version Scan${Color_Off}\n"
 		/usr/bin/nmap -T5 -p${ports} -sC -sV -oN vers-1 $host
